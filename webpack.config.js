e46b33d4902bd4d363aab79035a347cc0f39cd79
Miles Shebar <miles.shebar@gmail.com>
2018-03-27 23:19:03 -0400

    add jQuery as dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js'
   },
+  plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery",
+                d3: 'd3'
+            })
+        ],
   module: {
     rules: [
       {
