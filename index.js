a808a9e9fae5966e5a48b5fa0385fc37b094fbb0
Miles Shebar <miles.shebar@gmail.com>
2018-03-27 23:18:55 -0400

    add code from ApplePieGender.js

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
 import * as d3 from "d3";
 import 'd3pie';
+
+$(document).ready(function(){
+	//Change this to pie type
+	$("#ApplePieGender").empty();
+    d3.csv("thanksgiving-2015-poll-data.csv", function(data) {
+		var thanksCSV = data;
+		var maleCount = 0;
+		var femaleCount = 0;
+		for (var i = 0; i <= 1057; i++) {
+			//Change to proper question
+			if (thanksCSV[i]["Which type of pie is typically served at your Thanksgiving dinner? Please select all that apply. - Apple"] === "Apple") {
+				if (thanksCSV[i]["What is your gender?"] === "Male") {
+					maleCount++;
+				}
+				if (thanksCSV[i]["What is your gender?"] === "Female") {
+					femaleCount++;
+				}
+			}
+		}
+		//Change this to pie type
+		var pie = new d3pie("ApplePieGender", {
+			header: {
+				title: {
+					//Change title to pie type
+					text: "Apple Pie by Gender",
+					fontSize: 24,
+					font: "arial"
+				}
+			},
+			data: {
+				content: [
+					{ label: "Male", value: maleCount},
+					{ label: "Female", value: femaleCount }
+				]
+			},
+			labels: {
+				mainLabel: {
+					color: "#333333",
+					font: "arial",
+					fontSize: 16
+				},
+				percentage: {
+					color: "#dddddd",
+					font: "arial",
+					fontSize: 16,
+					decimalPlaces: 0
+				},
+				value: {
+					color: "#cccc44",
+					font: "arial",
+					fontSize: 16
+				}
+			},
+			misc: {
+				colors: {
+					segments: [
+						//Change colors here
+						"#ff8000", "#ff0000"
+					]
+				}
+			}
+		});
+	});
+});
