28713e57bba439b5e3e4b9a7e5adcae45c00a486
Emma Hood <hoode@kenyon.edu>
2018-04-01 22:26:33 -0400

    Added ChocolatePie Gender and Region js to index.js

--- index.js
+++ index.js
@@ -316,3 +316,158 @@ $(document).ready(function(){
 		$("#CherryPieRegion").append('<img src="piecrust.png" style="position:absolute;right:70px;top:75px;z-index:-1;width:365px">');
 	});
 });
+
+//ChocolatePieGender.js
+$(document).ready(function(){
+	$("#ChocolatePieGender").empty();
+	    d3.csv("thanksgiving-2015-poll-data.csv", function(data) {
+			var thanksCSV = data;
+			var maleCount = 0;
+			var femaleCount = 0;
+			for (var i = 0; i <= 1057; i++) {
+				//Change to proper question
+				if (thanksCSV[i]["Which type of pie is typically served at your Thanksgiving dinner? Please select all that apply. - Chocolate"] === "Chocolate") {
+					if (thanksCSV[i]["What is your gender?"] === "Male") {
+						maleCount++;
+					}
+					if (thanksCSV[i]["What is your gender?"] === "Female") {
+						femaleCount++;
+					}
+				}
+			}
+			//Change this to pie type
+			var pie = new d3pie("ChocolatePieGender", {
+				header: {
+					title: {
+						//Change title to pie type
+						text: "Chocolate Pie by Gender",
+						fontSize: 24,
+						font: "arial"
+					}
+				},
+				data: {
+					content: [
+						{ label: "Male", value: maleCount},
+						{ label: "Female", value: femaleCount }
+					]
+				},
+				labels: {
+					mainLabel: {
+						color: "#333333",
+						font: "arial",
+						fontSize: 16
+					},
+					percentage: {
+						color: "#dddddd",
+						font: "arial",
+						fontSize: 16,
+						decimalPlaces: 0
+					},
+					value: {
+						color: "#cccc44",
+						font: "arial",
+						fontSize: 16
+					}
+				},
+				misc: {
+					colors: {
+						segments: [
+							//Change colors here
+							"#ac7339", "#663300"
+						]
+					}
+				}
+			});
+			$("#ChocolatePieGender").append('<img src="piecrust.png" style="position:absolute;right:70px;top:75px;z-index:-1;width:365px">');
+		});
+});
+
+//ChocolatePieRegion.js
+$(document).ready(function(){
+	$("#ChocolatePieRegion").empty();
+	    d3.csv("thanksgiving-2015-poll-data.csv", function(data) {
+			var thanksCSV = data;
+			var midwestCount = 0;
+			var northeastCount = 0;
+			var southeastCount = 0;
+			var westCount = 0;
+			for (var i = 0; i <= 1057; i++) {
+				//Change to proper question
+				if (thanksCSV[i]["Which type of pie is typically served at your Thanksgiving dinner? Please select all that apply. - Chocolate"] === "Chocolate") {
+					if (thanksCSV[i]["US Region"] === "East North Central") {
+						midwestCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "East South Central") {
+						southeastCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "Middle Atlantic") {
+						northeastCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "Mountain") {
+						westCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "New England") {
+						northeastCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "Pacific") {
+						westCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "South Atlantic") {
+						southeastCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "West North Central") {
+						midwestCount++;
+					}
+					if (thanksCSV[i]["US Region"] === "West South Central") {
+						southeastCount++;
+					}
+				}
+			}
+			//Change this to pie type
+			var pie = new d3pie("ChocolatePieRegion", {
+				header: {
+					title: {
+						//Change title to pie type
+						text: "Chocolate Pie by Region",
+						fontSize: 24,
+						font: "arial"
+					}
+				},
+				data: {
+					content: [
+						{ label: "Midwest", value: midwestCount},
+						{ label: "Northeast", value: northeastCount},
+						{ label: "Southeast", value: southeastCount},
+						{ label: "West", value: westCount}
+					]
+				},
+				labels: {
+					mainLabel: {
+						color: "#333333",
+						font: "arial",
+						fontSize: 16
+					},
+					percentage: {
+						color: "#dddddd",
+						font: "arial",
+						fontSize: 16,
+						decimalPlaces: 0
+					},
+					value: {
+						color: "#cccc44",
+						font: "arial",
+						fontSize: 16
+					}
+				},
+				misc: {
+					colors: {
+						segments: [
+							//Change colors here
+							"#663300", "#86592d", "#ac7339", "#cc9966"
+						]
+					}
+				}
+			});
+			$("#ChocolatePieRegion").append('<img src="piecrust.png" style="position:absolute;right:70px;top:75px;z-index:-1;width:365px">');
+		});
+});
