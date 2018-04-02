8f718b566a78964f44e1be08287c1b107200e59e
Flynn F Shannon <shannon1@kenyon.edu>
2018-04-02 14:08:42 -0400

    put bar chart back in

--- index.js
+++ index.js
@@ -936,3 +936,46 @@ $(document).ready(function(){
 		$("#PumpkinPieRegion").append('<img src="piecrust.png" style="position:absolute;right:70px;top:75px;z-index:-1;width:365px">');
 	});
 });
+
+$(document).ready(function(){
+  $("#MainDishBars").empty();
+  d3.csv("thanksgiving-2015-poll-data.csv", function(data) {
+    var turkeyCount = 0;
+    var tofurkeyCount = 0;
+    var porkCount = 0;
+    var turduckenCount = 0;
+    var beefCount = 0;
+		console.log('initialized bar lengths');
+    data.forEach(function(person){
+      if(person["What is typically the main dish at your Thanksgiving dinner?"]==="Turkey"){
+        turkeyCount++;
+      }
+      else if(person["What is typically the main dish at your Thanksgiving dinner?"]==="Tofurkey"){
+        tofurkeyCount++;
+      }
+      else if(person["What is typically the main dish at your Thanksgiving dinner?"]==="Ham/Pork"){
+        porkCount++;
+      }
+      else if(person["What is typically the main dish at your Thanksgiving dinner?"]==="Roast beef"){
+        beefCount++;
+      }
+      else if(person["What is typically the main dish at your Thanksgiving dinner?"]==="Turducken"){
+        turduckenCount++;
+      }
+    });
+		console.log('turkey count: ' + turkeyCount);
+    var barLengths = [turkeyCount, tofurkeyCount, porkCount, turduckenCount, beefCount];
+  });
+});
+	console.log(`turkey count: ${turkeyCount}`);
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(barLengths)])
+    .range([0, 420]);
+
+d3.select(".MainDishBars")
+  .selectAll("div")
+    .data(barLengths)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
